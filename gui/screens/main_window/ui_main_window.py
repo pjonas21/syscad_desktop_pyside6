dc4f5f5d1b8e67c0811a5e43c6e8c228b5d69c8e
Paulo Jonas <pjonas21@gmail.com>
2023-06-01 19:33:35 -0300

    create buttons on left menu

--- gui/screens/main_window/ui_main_window.py
+++ gui/screens/main_window/ui_main_window.py
@@ -15,14 +15,16 @@ class UiMainWindow(object):
         # configurando frame central da aplicação
         self.central_frame = QFrame()
 
+
         # CRIANDO O LAYOUT PRINCIPAL DA APLICAÇÃO =========================================
         self.main_layout = QHBoxLayout(self.central_frame)
         self.main_layout.setContentsMargins(0,0,0,0)
         self.main_layout.setSpacing(0)
 
+
         # LEFT MENU =======================================================================
         self.left_menu = QFrame()
-        self.left_menu.setStyleSheet("background-color: #1E830E")
+        self.left_menu.setStyleSheet("background-color: #1E830E; color: #F3F2F2")
         self.left_menu.setMaximumWidth(50)
         self.left_menu.setMinimumWidth(50)
 
@@ -34,7 +36,21 @@ class UiMainWindow(object):
         # TOP FRAME MENU
         self.left_menu_top_frame = QFrame()
         self.left_menu_top_frame.setMinimumHeight(50)
-        self.left_menu_top_frame.setStyleSheet("background-color: black")
+
+        # LAYOUT MENU TOP FRAME
+        self.left_menu_top_layout = QVBoxLayout(self.left_menu_top_frame)
+        self.left_menu_top_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_top_layout.setSpacing(0)
+
+        # TOP BUTTONS MENU
+        self.toggle_button = QPushButton("Toggle")
+        self.btn_home = QPushButton("Início")
+        self.btn_colab = QPushButton("Colaboradores")
+
+        # ADD BUTTONS TO MENU TOP LAYOUT
+        self.left_menu_top_layout.addWidget(self.toggle_button)
+        self.left_menu_top_layout.addWidget(self.btn_home)
+        self.left_menu_top_layout.addWidget(self.btn_colab)
 
         # MENU SPACER
         self.left_menu_spacer = QSpacerItem(20,20, QSizePolicy.Minimum, QSizePolicy.Expanding)
@@ -42,7 +58,17 @@ class UiMainWindow(object):
         # BOTTOM FRAME MENU
         self.left_menu_bottom_frame = QFrame()
         self.left_menu_bottom_frame.setMinimumHeight(50)
-        self.left_menu_bottom_frame.setStyleSheet("background-color: black")
+
+        # LAYOUT MENU BOTTOM FRAME
+        self.left_menu_bottom_layout = QVBoxLayout(self.left_menu_bottom_frame)
+        self.left_menu_bottom_layout.setContentsMargins(0,0,0,0)
+        self.left_menu_bottom_layout.setSpacing(0)
+
+        # BOTTOM BUTTONS MENU
+        self.btn_settings = QPushButton("Config")
+
+        # ADD BUTTONS TO MENU BOTTOM LAYOUT
+        self.left_menu_bottom_layout.addWidget(self.btn_settings)
 
         # LABEL VERSION APP
         self.label_version = QLabel("v1.0.0")
@@ -56,6 +82,7 @@ class UiMainWindow(object):
         self.left_menu_layout.addItem(self.left_menu_spacer)
         self.left_menu_layout.addWidget(self.left_menu_bottom_frame)
         self.left_menu_layout.addWidget(self.label_version)
+
 
         # CONTENT =============================================================================
         self.content = QFrame()
@@ -89,6 +116,7 @@ class UiMainWindow(object):
         self.content_layout.setContentsMargins(0,0,0,0)
         self.content_layout.setSpacing(0)
 
+
         # FOOTER =============================================================================
         self.footer = QFrame()
         self.footer.setStyleSheet("background-color: #A2FD94; color: #56AB49")
@@ -111,11 +139,13 @@ class UiMainWindow(object):
         self.footer_layout.addItem(self.footer_spacer)
         self.footer_layout.addWidget(self.footer_label_right)
 
+
         # APPLICATION PAGES ================================================================
         self.pages = QStackedWidget()
         self.pages.setStyleSheet("font-size: 12pt; color: #083600")
         self.ui_pages = Ui_application_pages()
         self.ui_pages.setupUi(self.pages)
+
 
         # ADD WIDGETS CONTENT LAYOUT =======================================================
         self.content_layout.addWidget(self.top_bar)
@@ -126,6 +156,7 @@ class UiMainWindow(object):
         # ADD WIDGETS CENTRAL FRAME ========================================================
         self.main_layout.addWidget(self.left_menu)
         self.main_layout.addWidget(self.content)
+
 
         # SET CENTRAL FRAME ================================================================
         parent.setCentralWidget(self.central_frame)
