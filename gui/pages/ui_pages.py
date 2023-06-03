71bb3017f53f1c831f791f4f97beca8327d6a5fe
Paulo Jonas <pjonas21@gmail.com>
2023-06-03 13:02:29 -0300

    adding search page, for future implementations

--- gui/pages/ui_pages.py
+++ gui/pages/ui_pages.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'pagesTXPYVi.ui'
+## Form generated from reading UI file 'pageskNxNpN.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.5.1
 ##
@@ -15,6 +15,7 @@ class Ui_application_pages(object):
         if not application_pages.objectName():
             application_pages.setObjectName(u"application_pages")
         application_pages.resize(612, 363)
+        application_pages.setMinimumSize(QSize(600, 350))
         self.page_1 = QWidget()
         self.page_1.setObjectName(u"page_1")
         self.verticalLayout = QVBoxLayout(self.page_1)
@@ -83,11 +84,130 @@ class Ui_application_pages(object):
         self.page_2.setObjectName(u"page_2")
         self.verticalLayout_2 = QVBoxLayout(self.page_2)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.label_2 = QLabel(self.page_2)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setAlignment(Qt.AlignCenter)
+        self.frame_2 = QFrame(self.page_2)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setMinimumSize(QSize(600, 350))
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame_2)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.gridLayout_2 = QGridLayout()
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.field_search = QLineEdit(self.frame_2)
+        self.field_search.setObjectName(u"field_search")
+        self.field_search.setStyleSheet(u"QLineEdit {\n"
+"	padding: 6px;\n"
+"	border: 2px solid #56AB49;\n"
+"	border-radius: 8px;\n"
+"}")
 
-        self.verticalLayout_2.addWidget(self.label_2)
+        self.gridLayout_2.addWidget(self.field_search, 0, 0, 1, 1)
+
+        self.btn_search = QPushButton(self.frame_2)
+        self.btn_search.setObjectName(u"btn_search")
+        self.btn_search.setStyleSheet(u"QPushButton {\n"
+"	width: 80px;\n"
+"	font: 700 10pt \"Segoe UI\";\n"
+"	background-color: #1E830E;\n"
+"	color: #F3F2F2;\n"
+"	border-radius: 8px;\n"
+"	border: none;\n"
+"	padding: 6px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: #56AB49;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: #A2FD94;\n"
+"}")
+
+        self.gridLayout_2.addWidget(self.btn_search, 0, 1, 1, 1)
+
+
+        self.verticalLayout_5.addLayout(self.gridLayout_2)
+
+        self.gridLayout_4 = QGridLayout()
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.list_colab = QListView(self.frame_2)
+        self.list_colab.setObjectName(u"list_colab")
+        self.list_colab.setStyleSheet(u"QListView {\n"
+"	border-radius: 8px;\n"
+"	border: 2px solid #A2FD94;\n"
+"}")
+
+        self.gridLayout_4.addWidget(self.list_colab, 0, 0, 1, 1)
+
+        self.gridLayout_3 = QGridLayout()
+        self.gridLayout_3.setSpacing(0)
+        self.gridLayout_3.setObjectName(u"gridLayout_3")
+        self.btn_ficha = QPushButton(self.frame_2)
+        self.btn_ficha.setObjectName(u"btn_ficha")
+        self.btn_ficha.setStyleSheet(u"QPushButton {\n"
+"	width: 80px;\n"
+"	font: 700 10pt \"Segoe UI\";\n"
+"	background-color: #1E830E;\n"
+"	color: #F3F2F2;\n"
+"	border-radius: 8px;\n"
+"	border: none;\n"
+"	padding: 6px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: #56AB49;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: #A2FD94;\n"
+"}")
+
+        self.gridLayout_3.addWidget(self.btn_ficha, 2, 0, 1, 1)
+
+        self.btn_detail = QPushButton(self.frame_2)
+        self.btn_detail.setObjectName(u"btn_detail")
+        self.btn_detail.setStyleSheet(u"QPushButton {\n"
+"	width: 80px;\n"
+"	font: 700 10pt \"Segoe UI\";\n"
+"	background-color: #1E830E;\n"
+"	color: #F3F2F2;\n"
+"	border-radius: 8px;\n"
+"	border: none;\n"
+"	padding: 6px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: #56AB49;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: #A2FD94;\n"
+"}")
+
+        self.gridLayout_3.addWidget(self.btn_detail, 1, 0, 1, 1)
+
+        self.btn_add = QPushButton(self.frame_2)
+        self.btn_add.setObjectName(u"btn_add")
+        self.btn_add.setStyleSheet(u"QPushButton {\n"
+"	width: 80px;\n"
+"	font: 700 10pt \"Segoe UI\";\n"
+"	background-color: #1E830E;\n"
+"	color: #F3F2F2;\n"
+"	border-radius: 8px;\n"
+"	border: none;\n"
+"	padding: 6px;\n"
+"}\n"
+"QPushButton:hover {\n"
+"	background-color: #56AB49;\n"
+"}\n"
+"QPushButton:pressed {\n"
+"	background-color: #A2FD94;\n"
+"}")
+
+        self.gridLayout_3.addWidget(self.btn_add, 0, 0, 1, 1)
+
+
+        self.gridLayout_4.addLayout(self.gridLayout_3, 0, 1, 1, 1)
+
+
+        self.verticalLayout_5.addLayout(self.gridLayout_4)
+
+
+        self.verticalLayout_2.addWidget(self.frame_2)
 
         application_pages.addWidget(self.page_2)
         self.page_3 = QWidget()
@@ -113,7 +233,10 @@ class Ui_application_pages(object):
         self.lbl_user.setText("")
         self.lbl_user.setPlaceholderText(QCoreApplication.translate("application_pages", u"Informe o usu\u00e1rio...", None))
         self.btn_login.setText(QCoreApplication.translate("application_pages", u"Entrar", None))
-        self.label_2.setText(QCoreApplication.translate("application_pages", u"P\u00c1GINA 02", None))
+        self.btn_search.setText(QCoreApplication.translate("application_pages", u"Buscar", None))
+        self.btn_ficha.setText(QCoreApplication.translate("application_pages", u"Ficha Ind.", None))
+        self.btn_detail.setText(QCoreApplication.translate("application_pages", u"Detalhe", None))
+        self.btn_add.setText(QCoreApplication.translate("application_pages", u"Adicionar", None))
         self.label.setText(QCoreApplication.translate("application_pages", u"P\u00c1GINA 03", None))
     # retranslateUi
 
